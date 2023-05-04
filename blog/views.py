1eb7374020ba8bb597eb64cbe48aa51297e75f6f
Muhammed Shittu <devshittu@gmail.com>
2023-05-04 14:54:04 +0100

    View used get_object_or_404 instead of try/catch

--- blog/views.py
+++ blog/views.py
@@ -1,4 +1,4 @@
-from django.shortcuts import render
+from django.shortcuts import render, get_object_or_404
 from blog.models import Post
 
 
@@ -8,9 +8,5 @@ def post_list(request):
 
 
 def post_detail(request, id):
-    try:
-        post = Post.published.get(id=id)
-    except Post.DoesNotExist:
-        raise Http404("No Post found.")
-
-    return render(request, 'blog/post/detail.html', {'post':post})
+    post = get_object_or_404(Post, id=id, status=Post.Status.PUBLISHED)
+    return render(request, 'blog/post/detail.html', {'post': post})
