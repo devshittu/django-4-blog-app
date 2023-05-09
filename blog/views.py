93ec4caf4eccb3fcb40f43df5e8e24d09c932a1b
Muhammed Shittu <devshittu@gmail.com>
2023-05-09 23:51:36 +0100

    Included the paginator PageNotAnInteger

--- blog/views.py
+++ blog/views.py
@@ -1,4 +1,4 @@
-from django.core.paginator import Paginator, EmptyPage
+from django.core.paginator import Paginator, EmptyPage, PageNotAnInteger
 from django.shortcuts import render, get_object_or_404
 from blog.models import Post
 
@@ -9,6 +9,9 @@ def post_list(request):
     page_number = request.GET.get('page', 1)
     try:
         posts = paginator.page(page_number)
+    except PageNotAnInteger:
+        # If page_number is not an integer deliver the first page
+        posts = paginator.page(1)
     except EmptyPage:
         # If page_number is out of range deliver last page of results
         posts = paginator.page(paginator.num_pages)
