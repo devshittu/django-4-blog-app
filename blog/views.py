1d2b4a1a1e1afe7cc6acb5d23a92b7fb20f898c6
Muhammed Shittu <devshittu@gmail.com>
2023-05-09 23:50:24 +0100

    Included the paginator prop in list.html

--- blog/views.py
+++ blog/views.py
@@ -1,9 +1,17 @@
+from django.core.paginator import Paginator, EmptyPage
 from django.shortcuts import render, get_object_or_404
 from blog.models import Post
 
 
 def post_list(request):
     posts = Post.published.all()
+    paginator = Paginator(post_list, 3)
+    page_number = request.GET.get('page', 1)
+    try:
+        posts = paginator.page(page_number)
+    except EmptyPage:
+        # If page_number is out of range deliver last page of results
+        posts = paginator.page(paginator.num_pages)
     return render(request, 'blog/post/list.html', {'posts': posts})
 
 
